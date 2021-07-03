c7aa0c4242794acdfd33c59157d75f1636e4c782
esf <efrain.sotelo.ferry@gmail.com>
2021-07-03 19:18:50 +0200

    feat(corne 5x3):_

--- keyboards/crkbd/keymaps/esf/keymap.c
+++ keyboards/crkbd/keymaps/esf/keymap.c
@@ -49,41 +49,56 @@ enum macro_keycodes {
   KC_SAMPLEMACRO,
 };
 
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
+
+// 
+#define THU_SPC MT(MOD_RSFT, KC_SPC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3( \
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+
+      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+      XXXXXXX, HOME_A,   HOME_S, HOME_D,  HOME_F,    KC_G,                         KC_H,   HOME_J, HOME_K,  HOME_L, HOME_SCLN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL,   LOWER,  KC_ENT,     KC_SPC,  RAISE,  KC_RALT \
+      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+
+                                         XXXXXXX,   LOWER,  KC_ENT,     THU_SPC,  RAISE, XXXXXXX \
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,\
+     XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_TAB, MACRO1, XXXXXXX, KC_LGUI,  LGUI(KC_LALT), XXXXXXX,                 KC_VOLU, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT , KC_DEL,
+     XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI,  KC_LSFT, KC_VOLU,                     KC_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT , XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT, KC_UNDO,KC_CUT,  XXXXXXX,  KC_COPY, KC_PASTE ,                    KC_VOLD, KC_RGUI, XXXXXXX, KC_DOT,  XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, MACRO1 , KC_VOLD ,                    XXXXXXX,  KC_DEL, XXXXXXX, KC_DOT,  XXXXXXX,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LCTL, LOWER, KC_ENT ,    KC_SPC, RAISE, KC_RALT
+                                         XXXXXXX,   LOWER,  KC_ENT,     KC_BSPC,  RAISE,  XXXXXXX\
                                       //|--------------------------|  |--------------------------|
     ),
 
   [_RAISE] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     KC_ESC,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+     XXXXXXX,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_TAB,  KC_F1,  KC_F2,   KC_F3,   KC_F4,    KC_F5,                        KC_MINS, KC_EQL, KC_LBRC,  KC_RBRC, KC_PIPE, KC_GRV,
+     XXXXXXX, KC_GRV  , KC_QUOT , KC_TAB, KC_ESC, XXXXXXX,                      KC_MINS, KC_EQL, KC_LBRC,  KC_RBRC, KC_PIPE, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT, KC_F6,  KC_F7,   KC_F8,   KC_F9,    KC_F10,                       KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_BSLS, KC_TILD,
+     XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX,KC_TILD , XXXXXXX,                      KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_BSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, LOWER, KC_ENT,    KC_SPC, RAISE, KC_RALT
+                                         XXXXXXX,   LOWER,  KC_DEL,     KC_SPC,  RAISE,  XXXXXXX\
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -95,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
+                                         XXXXXXX,   LOWER,  KC_ENT,     KC_SPC,  RAISE,  XXXXXXX\
                                       //`--------------------------'  `--------------------------'
   )
 };
@@ -136,10 +151,10 @@ void set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
 const char *read_keylogs(void);
 
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
+/* const char *read_mode_icon(bool swap); */
+/* const char *read_host_led_state(void); */
+/* void set_timelog(void); */
+/* const char *read_timelog(void); */
 
 void matrix_scan_user(void) {
    iota_gfx_task();
@@ -150,10 +165,10 @@ void matrix_render_user(struct CharacterMatrix *matrix) {
     // If you want to change the display of OLED, you need to change here
     matrix_write_ln(matrix, read_layer_state());
     matrix_write_ln(matrix, read_keylog());
-    //matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
+    matrix_write_ln(matrix, read_keylogs());
+    /* matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui)); */
+    /* matrix_write_ln(matrix, read_host_led_state()); */
+    /* matrix_write_ln(matrix, read_timelog()); */
   } else {
     matrix_write(matrix, read_logo());
   }
@@ -230,12 +245,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           RGB_current_mode = rgblight_config.mode;
         }
       #endif
-    case MACRO1:
-        if (record->event.pressed){
-          SEND_STRING("Aerodinamica4");
+      #ifdef RGB_MATRIX_ENABLE
+        if (record->event.pressed) {
+          eeconfig_update_rgb_matrix_default();
+          rgb_matrix_enable();
         }
-        return false;
-    break;
+      #endif
+      break;
+    case MACRO1:
+      if (record->event.pressed){
+        SEND_STRING("Aerodinamica4");
+      }
+      return false;
   }
   return true;
 }
+
+#ifdef RGB_MATRIX_ENABLE
+
+void suspend_power_down_keymap(void) {
+    rgb_matrix_set_suspend_state(true);
+}
+
+void suspend_wakeup_init_keymap(void) {
+    rgb_matrix_set_suspend_state(false);
+}
+
+#endif
