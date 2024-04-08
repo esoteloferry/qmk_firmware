64dd02fe7c8d4986f1d5a7825ded40b147c3c4c3
esf <efrain.sotelo.ferry@gmail.com>
2024-04-08 12:43:57 +0200

    feat: v3

---
+++ keyboards/crkbd/keymaps/esf_v3/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no
+EXTRAKEY_ENABLE = yes
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = yes    # Enable VIA
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+WPM_ENABLE = yes
